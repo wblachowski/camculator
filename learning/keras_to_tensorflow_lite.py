c3a8f1f0c5c85fe8c1e93e0aa8fe75e8f1ae5309
Wojciech Blachowski <wblachowsky@gmail.com>
2019-08-21 20:47:29 +0200

    Refactor using argparse library

--- learning/keras_to_tensorflow_lite.py
+++ learning/keras_to_tensorflow_lite.py
@@ -1,5 +1,18 @@
+import argparse
+
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_keras_model_file( 'models/model.h5' )
-tflite_model = converter.convert()
-open( 'tensorflow_lite_model/model.tflite' , 'wb' ).write( tflite_model )
+parser = argparse.ArgumentParser(description='Convert model from h5 to tflite format.')
+parser.add_argument('-i', '--input', default='models/model.h5', help='path to h5 model')
+parser.add_argument('-o', '--output', default='models/model.tflite', help='target path to tflite model')
+
+
+def convert(input_path, output_path):
+    converter = tf.lite.TFLiteConverter.from_keras_model_file(input_path)
+    tflite_model = converter.convert()
+    open(output_path, 'wb').write(tflite_model)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    convert(args.input, args.output)
