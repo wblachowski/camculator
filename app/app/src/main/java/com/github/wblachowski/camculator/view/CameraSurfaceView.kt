21ea0d38e7c854199a6a20d9b8740f5b254f72fd
Wojciech Blachowski <wblachowsky@gmail.com>
2021-04-14 14:18:09 +0200

    Refactor getting optimal preview size

--- app/app/src/main/java/com/github/wblachowski/camculator/view/CameraSurfaceView.kt
+++ app/app/src/main/java/com/github/wblachowski/camculator/view/CameraSurfaceView.kt
@@ -1,30 +1,21 @@
 package com.github.wblachowski.camculator.view
 
-import android.content.ContentValues.TAG
 import android.hardware.Camera
-import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import com.github.wblachowski.camculator.activity.MainActivity
+import kotlin.math.abs
 
-class CameraSurfaceView(private val mainActivity: MainActivity, private var camera: Camera) : SurfaceView(mainActivity), SurfaceHolder.Callback {
-
-    var prSupportedPreviewSizes: List<Camera.Size>? = null
+class CameraSurfaceView(private val mainActivity: MainActivity, var camera: Camera) : SurfaceView(mainActivity), SurfaceHolder.Callback {
 
     init {
         holder.addCallback(this)
-        // deprecated setting, but required on Android versions prior to 3.0
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
-        prSupportedPreviewSizes = camera.parameters.supportedPreviewSizes
-
     }
 
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        startCameraPreview()
-    }
+    override fun surfaceCreated(holder: SurfaceHolder) = startCameraPreview()
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
-        // empty. Take care of releasing the Camera preview in your activity.
+        // Empty. Releasing the camera in the main activity.
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
@@ -34,53 +25,29 @@ class CameraSurfaceView(private val mainActivity: MainActivity, private var came
         try {
             camera.stopPreview()
         } catch (e: Exception) {
-            // ignore: tried to stop a non-existent preview
+            // Ignoring, tried to stop a non-existent preview
         }
         startCameraPreview()
     }
 
     private fun startCameraPreview() {
-        try {
-            camera.setDisplayOrientation(90)
-            //set camera to continually auto-focus
-            val params = camera.parameters
-            params.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
-            camera.parameters = params
-            camera.setPreviewCallback(mainActivity::onPreviewFrame)
-            camera.setPreviewDisplay(holder)
-            camera.startPreview()
-        } catch (e: Exception) {
-            Log.d(TAG, "Error starting camera preview: " + e.message)
+        camera.apply {
+            parameters = camera.parameters.apply {
+                focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
+            }
+            setDisplayOrientation(90)
+            setPreviewCallback(mainActivity::onPreviewFrame)
+            setPreviewDisplay(holder)
+            startPreview()
         }
     }
 
-    fun setCamera(camera : Camera){
-        this.camera=camera
-    }
-
-    fun getOptimalPreviewSize(sizes: List<Camera.Size>?, w: Int, h: Int): Camera.Size? {
-        val ASPECT_TOLERANCE = 0.1
+    fun getOptimalPreviewSize(w: Int, h: Int, aspectTolerance: Float = 0.1f): Camera.Size? {
+        val supportedPreviewSizes = camera.parameters.supportedPreviewSizes
         val targetRatio = h.toDouble() / w
-        if (sizes == null) return null
-        var optimalSize: Camera.Size? = null
-        var minDiff = Double.MAX_VALUE
-        for (size in sizes) {
-            val ratio = size.width.toDouble() / size.height
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue
-            if (Math.abs(size.height - h) < minDiff) {
-                optimalSize = size
-                minDiff = Math.abs(size.height - h).toDouble()
-            }
-        }
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE
-            for (size in sizes) {
-                if (Math.abs(size.height - h) < minDiff) {
-                    optimalSize = size
-                    minDiff = Math.abs(size.height - h).toDouble()
-                }
-            }
-        }
-        return optimalSize
+
+        return supportedPreviewSizes
+                .filter { abs(it.width.toDouble() / it.height - targetRatio) <= aspectTolerance }
+                .minBy { abs(it.height - h) } ?: supportedPreviewSizes.minBy { abs(it.height - h) }
     }
 }
