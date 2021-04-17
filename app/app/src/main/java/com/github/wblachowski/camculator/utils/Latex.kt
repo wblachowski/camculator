0f712140ee66cc074b1a7a1c5047e01df598b3f4
Wojciech Blachowski <wblachowsky@gmail.com>
2021-04-17 15:03:17 +0200

    Add more latex support

--- app/app/src/main/java/com/github/wblachowski/camculator/utils/Latex.kt
+++ app/app/src/main/java/com/github/wblachowski/camculator/utils/Latex.kt
@@ -2,5 +2,6 @@ package com.github.wblachowski.camculator.utils
 
 fun latexify(mathText: String) =
         mathText.replace("I", "i")
-                .replace("""([^+\-=]+)/([^+\-=\*]+)""".toRegex(), "\\\\frac{$1}{$2}")
+                .replace("""\^(\d+)""".toRegex(), "^{$1}")
+                .replace("""([^+\-=]+)/([^+\-=*]+)""".toRegex(), "\\\\frac{$1}{$2}")
                 .replace("*", " \\cdot ")
