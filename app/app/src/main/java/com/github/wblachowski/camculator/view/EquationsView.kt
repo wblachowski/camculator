ba4afdbe946839c2bf80cb0db94144dc1f4238e4
Wojciech Blachowski <wblachowsky@gmail.com>
2021-04-19 23:46:46 +0200

    Refactor strings

--- app/app/src/main/java/com/github/wblachowski/camculator/view/EquationsView.kt
+++ app/app/src/main/java/com/github/wblachowski/camculator/view/EquationsView.kt
@@ -3,11 +3,11 @@ package com.github.wblachowski.camculator.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.RelativeLayout
 import com.github.wblachowski.camculator.R
 import com.github.wblachowski.camculator.processing.model.result.equation.EquationProcessingResult
 import kotlinx.android.synthetic.main.equations_view.view.*
-
 
 class EquationsView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -18,7 +18,13 @@ class EquationsView @JvmOverloads constructor(
 
     fun updateEquations(result: EquationProcessingResult) {
         equationsTitle.text = if (result.correct) "Equations" else "Equations (incorrect)"
+        equationsTitle.text = when {
+            result.equations.isEmpty() -> context.getString(R.string.equations_not_found)
+            result.correct -> context.getString(R.string.equations)
+            else -> context.getString(R.string.equations_incorrect)
+        }
         equationsTitle.setTextColor(if (result.correct) resources.getColor(R.color.white) else resources.getColor(R.color.red))
+        equationsMathView.visibility = if (result.equations.isEmpty()) View.INVISIBLE else View.VISIBLE
         if (result.latexEquations != equationsMathView.getText()) {
             equationsMathView.setText(result.latexEquations)
         }
