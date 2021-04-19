034b98ba544302652251ec746d5411c0f0c26589
Wojciech Blachowski <wblachowsky@gmail.com>
2021-04-19 22:50:32 +0200

    Refactor image processing task types

--- app/app/src/main/java/com/github/wblachowski/camculator/processing/ImageProcessingTask.kt
+++ app/app/src/main/java/com/github/wblachowski/camculator/processing/ImageProcessingTask.kt
@@ -2,22 +2,22 @@ package com.github.wblachowski.camculator.processing
 
 import android.os.AsyncTask
 import com.github.wblachowski.camculator.processing.model.Payload
+import com.github.wblachowski.camculator.processing.model.result.ProcessingResult
 import com.github.wblachowski.camculator.processing.processor.BoxesProcessor
 import com.github.wblachowski.camculator.processing.processor.EquationInterpreter
 import com.github.wblachowski.camculator.processing.processor.ImageProcessor
-import com.github.wblachowski.camculator.processing.model.result.ProcessingResult
 import org.opencv.core.Size
 
 
-class ImageProcessingTask(val onPostProcessing: (ProcessingResult) -> Unit) : AsyncTask<Any, Void, ProcessingResult>() {
+class ImageProcessingTask(val onPostProcessing: (ProcessingResult) -> Any) : AsyncTask<Payload, Void, ProcessingResult>() {
 
     private lateinit var payload: Payload
     private val imageProcessor = ImageProcessor.getInstance()
     private val equationInterpreter = EquationInterpreter.getInstance()
     private val boxesProcessor = BoxesProcessor.getInstance()
 
-    override fun doInBackground(objects: Array<Any>): ProcessingResult {
-        payload = objects.first() as Payload
+    override fun doInBackground(objects: Array<Payload>): ProcessingResult {
+        payload = objects.first()
         val imageResult = imageProcessor.process(payload.bitmap)
         val equationResult = equationInterpreter.findEquations(imageResult.symbols)
         val finalSize = Size(payload.cropRectangle.width().toDouble(), payload.cropRectangle.height().toDouble())
@@ -25,5 +25,7 @@ class ImageProcessingTask(val onPostProcessing: (ProcessingResult) -> Unit) : As
         return ProcessingResult(imageResult, equationResult, boxesBitmap)
     }
 
-    override fun onPostExecute(result: ProcessingResult) = onPostProcessing(result)
+    override fun onPostExecute(result: ProcessingResult) {
+        onPostProcessing(result)
+    }
 }
