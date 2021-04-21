95ca0db04e831c3ffe83f1830f3c6551dfc1b50f
Wojciech Blachowski <wblachowsky@gmail.com>
2021-04-21 20:13:15 +0200

    Fix problems with exponentation in latex formulas

--- app/app/src/main/java/com/github/wblachowski/camculator/utils/Latex.kt
+++ app/app/src/main/java/com/github/wblachowski/camculator/utils/Latex.kt
@@ -3,5 +3,26 @@ package com.github.wblachowski.camculator.utils
 fun latexify(mathText: String) =
         mathText.replace("I", "i")
                 .replace("""([^+\-=]+)/([^+\-=*]+)""".toRegex(), "\\\\frac{$1}{$2}")
-                .replace("""\^([^+\-=/*\}\{\\]+)""".toRegex(), "^{$1}")
+                .let { addExpBrackets(it) }
                 .replace("*", " \\cdot ")
+
+private fun addExpBrackets(text: String): String {
+    var opened = 0
+    var newText = ""
+    val terminating = listOf('+', '-', '=', '/', '*', '\\', ']', '{', '}')
+    for (char in text) {
+        if (char == '^') {
+            newText += "^{"
+            opened++
+        } else if (opened > 0 && char in terminating) {
+            newText += "$char}"
+            opened--
+        } else {
+            newText += char
+        }
+    }
+    for (i in 0 until opened) {
+        newText += "}"
+    }
+    return newText
+}
